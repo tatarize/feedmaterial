38a176f9a36732dcd63eb83ca667ee7283de9298
tatarize <tatarize@gmail.com>
2023-12-10 07:08:18 -0800

    Add timeout, delay, and estop on fail.

--- feed/main.py
+++ feed/main.py
@@ -1,40 +1,52 @@
 import time
-
 import serial
-
-
-def communicate_with_arduino(port, baud_rate, channel=print):
-    arduino = None
-    try:
-        # Open the serial port
-        channel("Connecting")
-        arduino = serial.Serial(port, baud_rate, timeout=2)
-        channel("Connected, waiting 5 seconds.")
-        time.sleep(5.0)
-
-        channel("Writing Start.")
-        # Send the "Start" command
-        arduino.write(b"Start\n")
-
-        # Read and print the response until "End" is received
-        while True:
-            response = arduino.readline().decode("utf-8").strip()
-            channel(response)
-
-            if "end" in response.lower():
-                break
-
-    except Exception as e:
-        channel(f"Error: {e}")
-    finally:
-        # Close the serial port
-        if arduino is not None and arduino.is_open:
-            arduino.close()
 
 
 def plugin(kernel, lifecycle):
     if lifecycle == "register":
-        @kernel.console_option('com_port', 'c', type=str, default="COM4")
+        @kernel.console_option("port", "p", type=str, default="COM4")
+        @kernel.console_option(
+            "baud_rate", "b", type=int, default=9600, help="baud rate"
+        )
+        @kernel.console_option(
+            "timeout", "t", type=float, default=20.0, help="timeout in seconds"
+        )
+        @kernel.console_option(
+            "delay", "d", type=float, default=5.0, help="Wait time before sending data info."
+        )
         @kernel.console_command('feedmaterial', help="Feed Material.")
-        def example_cmd(command, channel, _, com_port="COM4", **kwargs):
-            communicate_with_arduino(com_port, 9600, channel=channel)
+        def feedmaterial_cmd(channel, _, port="COM4", baud_rate=9600, timeout=20.0, delay=5.0, **kwargs):
+            arduino = None
+            try:
+                # Open the serial port
+                channel("Connecting")
+                arduino = serial.Serial(port, baud_rate, timeout=2)
+                channel(f"Connected, waiting {delay} seconds.")
+                time.sleep(delay)
+
+                channel("Writing Start.")
+                # Send the "Start" command
+                arduino.write(b"Start\n")
+
+                # Read and print the response until "End" is received
+                found = False
+                end_time = time.time() + timeout
+                while end_time > time.time():
+                    response = arduino.readline().decode("utf-8").strip()
+                    channel(response)
+
+                    if "end" in response.lower():
+                        found = True
+                        break
+                    if "failed" in response.lower():
+                        break
+                if not found:
+                    channel("Timeout Reached or Failed, aborting...")
+                    kernel.console("estop\n")
+
+            except Exception as e:
+                channel(f"Error: {e}")
+            finally:
+                # Close the serial port
+                if arduino is not None and arduino.is_open:
+                    arduino.close()
